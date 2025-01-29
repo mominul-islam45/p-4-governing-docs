2376ad8f882ef5ce77ca480602ebba8babd8c90f
mominul-islam45 <mominul4586@gmail.com>
2025-01-29 15:18:20 +0600

    react environment setup done by vite with tailwindcss 4.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline font-grotesk  bg-linear-(--gradient-linear-primary)">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
