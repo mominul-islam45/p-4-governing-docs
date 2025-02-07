33175aaf539aaaa60464bb595d6a7665b18584a9
mominul-islam45 <mominul4586@gmail.com>
2025-02-07 15:44:24 +0600

    cn fun added

--- src/lib/utilities/index.js
+++ src/lib/utilities/index.js
@@ -1,4 +1,4 @@
 import { twMerge } from 'tailwind-merge'
 import {clsx} from 'clsx'
 
-export const cn = (...inputs) => twMerge(clsx(...inputs)); 
+export const cn = (...inputs) => twMerge(clsx(...inputs));
